5a3c742ff02fe7c2f283360345ad32dc6ac7c718
Kevin Robbins <kevin.robbins99@gmail.com>
2018-03-25 18:30:54 -0400

    tutorial part 5 - generic views

--- tutorialsite1/polls/views.py
+++ tutorialsite1/polls/views.py
@@ -6,6 +6,21 @@ from django.views import generic
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.ListView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]

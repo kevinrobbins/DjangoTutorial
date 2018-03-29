915874f4b3d146a263c6076880592644348b1080
Kevin Robbins <kevin.robbins99@gmail.com>
2018-03-28 20:41:10 -0400

    tutorial pt 7

--- tutorialsite1/tutorialsite1/settings.py
+++ tutorialsite1/tutorialsite1/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'tutorialsite1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
